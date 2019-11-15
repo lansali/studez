67952df271b00334a954b366b741e2ecfba11122
Sylvance <kerandisylvance@gmail.com>
2019-11-15 17:36:53 +0300

    Add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+    helper_method :logged_in?
+
+    before_action :authenticate_request
+
+    def current_user
+        if session[:user_id]
+            @current_user ||= User.find(session[:user_id])
+        else
+            @current_user = nil
+        end
+    end
+
+    def logged_in?
+        !current_user.nil?  
+    end
+
+    private
+
+    def authenticate_request
+        if !logged_in?
+            redirect_to login_path, notice: "Log in first to get access!"
+        end
+    end
 end
